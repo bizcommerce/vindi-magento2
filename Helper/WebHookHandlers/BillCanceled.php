1fc4a1e04d7bcddf7fa8f927f972a65a9d8b2940
Iago Cedran <iago@bizcommerce.com.br>
2025-03-10 18:34:34 +0000

    feat: refund charged payment method in multipayments

--- Helper/WebHookHandlers/BillCanceled.php
+++ Helper/WebHookHandlers/BillCanceled.php
@@ -1,128 +1,103 @@
 <?php
-
 namespace Vindi\Payment\Helper\WebHookHandlers;
 
+use Magento\Framework\Exception\LocalizedException;
+use Psr\Log\LoggerInterface;
+use Vindi\Payment\Model\PaymentSplitFactory;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Vindi\Payment\Model\Payment\Charge;
+
+/**
+ * Class BillCanceled
+ *
+ * Handles the bill canceled webhook event.
+ */
 class BillCanceled
 {
     /**
-     * @var \Vindi\Payment\Model\Payment\Bill
-     */
-    protected $bill;
-
-    /**
-     * @var \Vindi\Payment\Helper\WebHookHandlers\Order
-     */
-    protected $order;
-
-    /**
-     * @var \Psr\Log\LoggerInterface
+     * @var LoggerInterface
      */
     protected $logger;
 
     /**
-     * @var \Magento\Sales\Api\OrderRepositoryInterface
+     * @var PaymentSplitFactory
+     */
+    protected $paymentSplitFactory;
+
+    /**
+     * @var OrderRepositoryInterface
      */
     protected $orderRepository;
 
     /**
-     * @var \Magento\Framework\Api\SearchCriteriaBuilder
+     * @var Charge
      */
-    protected $searchCriteriaBuilder;
+    protected $charge;
 
+    /**
+     * Constructor.
+     *
+     * @param LoggerInterface $logger
+     * @param PaymentSplitFactory $paymentSplitFactory
+     * @param OrderRepositoryInterface $orderRepository
+     * @param Charge $charge
+     */
     public function __construct(
-        \Magento\Sales\Api\OrderRepositoryInterface $orderRepository,
-        \Vindi\Payment\Model\Payment\Bill $bill,
-        \Vindi\Payment\Helper\WebHookHandlers\Order $order,
-        \Psr\Log\LoggerInterface $logger,
-        \Magento\Framework\Api\SearchCriteriaBuilder $searchCriteriaBuilder
+        LoggerInterface $logger,
+        PaymentSplitFactory $paymentSplitFactory,
+        OrderRepositoryInterface $orderRepository,
+        Charge $charge
     ) {
-        $this->orderRepository = $orderRepository;
-        $this->bill = $bill;
-        $this->order = $order;
         $this->logger = $logger;
-        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->paymentSplitFactory = $paymentSplitFactory;
+        $this->orderRepository = $orderRepository;
+        $this->charge = $charge;
     }
 
     /**
-     * @param array $data
+     * Process bill canceled webhook event.
      *
+     * @param array $data Webhook event data.
      * @return bool
-     * @throws \Exception
+     * @throws LocalizedException
      */
     public function billCanceled($data)
     {
-        $bill = $data['bill'];
-
-        if (!$bill) {
-            $this->logger->error(__('Error while interpreting webhook "bill_canceled"'));
-            return false;
+        $billId = isset($data['id']) ? $data['id'] : null;
+        if (!$billId) {
+            throw new LocalizedException(__('Bill ID not found in webhook data.'));
         }
-
-        $isSubscription = isset($bill['subscription']['id']);
-        $order = null;
-
-        if ($isSubscription) {
-            $order = $this->getOrderByVindiBillAndSubscriptionId($bill['id'], $bill['subscription']['id']);
+        $paymentSplitCollection = $this->paymentSplitFactory->create()->getCollection()
+            ->addFieldToFilter('bill_id', $billId);
+        if ($paymentSplitCollection->getSize() > 0) {
+            $paymentSplitItems = $paymentSplitCollection->getItems();
+            foreach ($paymentSplitItems as $paymentSplit) {
+                if (!$paymentSplit->getIsRefunded()) {
+                    $refundResult = $this->charge->refund($paymentSplit->getBillId(), ['amount' => $paymentSplit->getAmount()]);
+                    if ($refundResult) {
+                        $paymentSplit->setIsRefunded(1);
+                        $paymentSplit->setRefundAmount($paymentSplit->getAmount());
+                        $paymentSplit->setRefundDate(date('Y-m-d H:i:s'));
+                        $paymentSplit->save();
+                    }
+                }
+            }
+            $firstPaymentSplit = reset($paymentSplitItems);
+            $orderId = $firstPaymentSplit->getOrderId();
+            try {
+                $order = $this->orderRepository->get($orderId);
+                if ($order->canCancel()) {
+                    $order->cancel();
+                    $order->addStatusHistoryComment(__('Order canceled due to multi-method payment refund.'));
+                    $this->orderRepository->save($order);
+                }
+            } catch (\Exception $e) {
+                $this->logger->error(__('Error canceling order: %1', $e->getMessage()));
+            }
+            return true;
+        } else {
+            $this->logger->info(__('Bill canceled event processed for single-method payment.'));
+            return true;
         }
-
-        if (!$order) {
-            $order = $this->getOrderFromBill($bill['id']);
-        }
-
-        if (!$order) {
-            $this->logger->warning(__('Order not found'));
-            return false;
-        }
-
-        $order->cancel();
-        $order->addStatusHistoryComment(__(sprintf(
-            'Vindi API: Order %s Canceled.',
-            $order->getId()
-        )));
-        $this->orderRepository->save($order);
-
-        $this->logger->info(__(sprintf(
-            'Vindi API: Order %s Canceled.',
-            $order->getId()
-        )));
-
-        return true;
-    }
-
-    private function getOrderFromBill($billId)
-    {
-        $bill = $this->bill->getBill($billId);
-
-        if (!$bill) {
-            return false;
-        }
-
-        return $this->order->getOrder(compact('bill'));
-    }
-
-    /**
-     * @param $vindiBillId
-     * @param $subscriptionId
-     * @return bool|\Magento\Sales\Api\Data\OrderInterface
-     */
-    private function getOrderByVindiBillAndSubscriptionId($vindiBillId, $subscriptionId)
-    {
-        $searchCriteria = $this->searchCriteriaBuilder
-            ->addFilter('vindi_bill_id', $vindiBillId, 'eq')
-            ->addFilter('vindi_subscription_id', $subscriptionId, 'eq')
-            ->create();
-
-        $orderList = $this->orderRepository
-            ->getList($searchCriteria)
-            ->getItems();
-
-        try {
-            return reset($orderList);
-        } catch (\Exception $e) {
-            $this->logger->error(__('Order with Vindi Bill ID #%1 and Subscription ID #%2 not found', $vindiBillId, $subscriptionId));
-            $this->logger->error($e->getMessage());
-        }
-
-        return false;
     }
 }
